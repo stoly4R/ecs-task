39abe16bc5c5f3e1012e142c4b1ab844604a20f4
stoly4R <root@localhost.localdomain>
2021-09-06 05:34:17 -0400

    adding deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,31 @@
-#!/bin/bash
-
-ARCH="amd64"
-TERRAFORM_VERSION="0.13.0"
-OS="linux"
-
-if test -f "terraform"; then
-    echo "terraform binary exists"
-    ./terraform init
-else
-    tar -zxvf 
-fi
+while test $# -gt 0; do
+  case "$1" in
+    -h|--help)
+      echo "options:"
+      echo "-h, --help                show brief help"
+      echo "--deploy,                 will run terraform apply and create environment based on your code"
+      echo "--remove,                 will run terraform destroy and remove created environment"
+      exit 0
+      ;;
+    --deploy)
+      shift
+      if test -f "terraform"; then
+      echo "terraform binary exists"
+      ./terraform init
+      ./terraform apply -auto-approve
+      else
+      tar -zxvf terraform_0.13.0_linux_amd64.tar.gz
+      ./terraform init
+      fi
+      shift
+      ;;
+    --remove)
+      shift
+      ./terraform destroy -auto-approve
+      shift
+      ;;
+    *)
+      break
+      ;;
+  esac
+done
